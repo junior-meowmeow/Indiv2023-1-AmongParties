70a7378c2eabc4856ba19edf3ac223371bb8740e
Pathorn Roungtan <junior254789@gmail.com>
2023-12-07 20:45:13 +0700

    Add Description to PickableObject

--- Assets/Scripts/Object/PickableObject.cs
+++ Assets/Scripts/Object/PickableObject.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class PickableObject : SpawnableObject
 {
+    [Header("Object Info")]
+    public string objectName = "Pickable Object";
+    [TextArea(6, 10)]
+    public string description = "Press E to DROP\nPress MOUSE1 to DROP";
     [Header("Base Property")]
     public ObjectType objectType;
     public ObjectColor objectColor;
@@ -62,8 +66,7 @@ public class PickableObject : SpawnableObject
 
     private void UpdatePositionNoRB()
     {
-        transform.position = holdPlayer.holdPos.position;
-        transform.rotation = holdPlayer.holdPos.rotation;
+        transform.SetPositionAndRotation(holdPlayer.holdPos.position, holdPlayer.holdPos.rotation);
     }
 
     public virtual void Use(bool isHolding)
